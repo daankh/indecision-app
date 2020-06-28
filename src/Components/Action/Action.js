6d927f8eef0cd3821847f0c78907a6102cbe2811
daankh <jarosz.dan@gmail.com>
2020-06-28 15:59:25 +0200

    feat(Action): convert to functional component

--- src/Components/Action/Action.js
+++ src/Components/Action/Action.js
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Action extends Component {
-  render() {
-    const { hasOptions, handleMakeDecision, handleDeleteOptions } = this.props;
-
-    return (
-      <div>
-        <p>{hasOptions ? "Here are your options" : "No options"}</p>
-        <button disabled={!hasOptions} onClick={handleMakeDecision}>
-          What should I do?
-        </button>
-        <button onClick={handleDeleteOptions}>Remove all</button>
-      </div>
-    );
-  }
-}
+const Action = ({ hasOptions, handleMakeDecision, handleDeleteOptions }) => {
+  return (
+    <div>
+      <p>{hasOptions ? "Here are your options" : "No options"}</p>
+      <button disabled={!hasOptions} onClick={handleMakeDecision}>
+        What should I do?
+      </button>
+      <button onClick={handleDeleteOptions}>Remove all</button>
+    </div>
+  );
+};
 
 export default Action;
