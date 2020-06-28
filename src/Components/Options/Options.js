2799c2906f43fa1bec94a931124a1ac3d103bf5c
daankh <jarosz.dan@gmail.com>
2020-06-28 15:59:42 +0200

    feat(Options): convert to functional compoennt

--- src/Components/Options/Options.js
+++ src/Components/Options/Options.js
@@ -1,23 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import Option from "./components/Option";
 
-class Options extends Component {
-  render() {
-    const { options, handleRemoveOption } = this.props;
-
-    return (
-      <ol>
-        {options.map((option, index) => (
-          <Option
-            key={index}
-            index={index}
-            option={option}
-            handleRemoveOption={handleRemoveOption}
-          />
-        ))}
-      </ol>
-    );
-  }
-}
+const Options = ({ options, handleRemoveOption }) => {
+  return (
+    <ol>
+      {options.map((option, index) => (
+        <Option
+          key={index}
+          index={index}
+          option={option}
+          handleRemoveOption={handleRemoveOption}
+        />
+      ))}
+    </ol>
+  );
+};
 
 export default Options;
