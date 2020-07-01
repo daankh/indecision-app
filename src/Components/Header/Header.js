7b5bac64959637336c86c142e7051504bdcca125
daankh <jarosz.dan@gmail.com>
2020-07-01 22:39:36 +0200

    styles: prepare sass and style header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = ({ title, subtitle }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      {subtitle && <p>{subtitle}</p>}
+    <div className="header">
+      <h1 className="header__title">{title}</h1>
+      {subtitle && <p className="header__subtitle">{subtitle}</p>}
     </div>
   );
 };
